0d20b2e538814a7f43a2d08fadc88a1380598367
Jhonatan Gomes Moura <166958516+jhonatangomes@users.noreply.github.com>
2024-09-30 22:53:15 -0300

    atualização

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
 
-let boxBuscar = document.querySelector('.buscar-box');
-let lupa = document.querySelector('.lupa-buscar');
-let btnFechar = document.querySelector('.btn-fechar');
-
-lupa.addEventListener('click', ()=> {
-    boxBuscar.classList.add('ativar')
-})
-
-btnFechar.addEventListener('click', ()=> {
-    boxBuscar.classList.remove('ativar')
-})
-
-
 
 function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
